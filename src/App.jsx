543f17fcb93ecc939865f0ffaa0dc3e701c86c89
Mihkaj <mihka.jh02@gmail.com>
2026-05-04 13:37:56 +0200

    feat: initialize project with React, Vite, and custom CSS styling

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './index.css';
+
+function App() {
+  return (
+    <section>
+        <div className="container">
+            <div className="blob"></div>           
+        </div>
+        <header><h1>Mihka Helander</h1></header>
+        <div className="content">
+            <p>Have you ever seen a man who is an expert at singing, being handsome, <br /> programming and 3D modelling? <br /> No? Me neither.</p>
+            <p>But I think I have found the next best thing!<br /> Let me present<br /> Mihka Helander!</p>
+        </div>
+    </section>
+  );
+}
+
+export default App;
